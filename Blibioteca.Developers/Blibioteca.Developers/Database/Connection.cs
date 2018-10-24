207fa7a3e0f1fb2a06d6088fdcd47d2c72881a64
Pedro Henrique <PHM1080@outlook.com>
2018-10-24 00:44:50 -0300

    Adicionado componentes Database

---
+++ Blibioteca.Developers/Blibioteca.Developers/Database/Connection.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blibioteca.Developers.Database
+{
+    class Connection
+    {
+        public MySqlConnection Create()
+        {
+            string connectionString = "server=ENDEREÇO;database=NOMEDOBANCO;uid=LOGIN;password=SENHA;sslmode=none";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
--- Software.Basico/Software.Basico/frmPrincipal.Designer.cs
+++ Software.Basico/Software.Basico/frmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Software.Basico
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variável de designer necessária.
